023a9faff549ef045daaa3c7db6b458829d35590
ddiehl <daniel.diehl@gmail.com>
2016-06-21 00:31:30 -0400

    initial add

---
+++ spring-boot-web-test/src/main/java/com/diehl/dummy/SpringBootWebTestApplication.java
@@ -0,0 +1,12 @@
+package com.diehl.dummy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebTestApplication.class, args);
+	}
+}
